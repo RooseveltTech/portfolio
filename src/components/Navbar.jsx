6b19df4ec81ccc2aa5be2f7f60dddfbaaf886cdc
RooseveltTech <rooseveltabandy@gmail.com>
2023-12-13 16:13:24 +0100

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,8 +45,8 @@ const Navbar = () => {
         >
           {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Dev.Edmund &nbsp;
-            <span className="sm:block hidden"> | Frontend Dev</span>
+            Dev_Roosevelt &nbsp;
+            <span className="sm:block hidden"> | Backend Dev</span>
           </p>
         </Link>
 
